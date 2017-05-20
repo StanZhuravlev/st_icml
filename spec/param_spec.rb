eaf7a303271a201c49f67abaa8933dae7a96b1f6
StanZhuravlev <stan@post-api.ru>
2017-05-21 01:10:23 +0300

    Initial commit

--- spec/param_spec.rb
+++ spec/param_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
-describe Param do
-  pending "write it"
+RSpec.describe ::StICML::Param do
+
+  describe "::StICML::Param" do
+    it "Корректные значения после конструктора" do
+      item = ::StICML::Param.new
+      item.name = 'Размер'
+      item.code = 'size'
+      item.value = 'Большой размер'
+      expect(item.to_icml).to include('param\sname\="Размер"')
+    end
+  end
+
 end
